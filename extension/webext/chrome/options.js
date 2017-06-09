a37a51c9064e283a185ae1bc5b83dc7e8065c2e1
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-06-09 23:08:54 +0200

    Begin work on the WebExtension version

---
+++ extension/webext/chrome/options.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Saves options to browser.storage.local.
+ *
+ * @param {Number} stack_depth
+ * @returns {undefined}
+ */
+function save_options() {
+  browser.storage.local.set({
+    playerSettings: {
+      hud: document.getElementById('hud').checked
+    }
+  }).then(() => {
+    console.info("[Shumway]", "Sucessfully saved options.");
+  }).catch((error) => {
+    console.error("[Shumway]", "Couldn't save options.", error);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in browser.storage.
+function restore_options() {
+  browser.storage.local.get({
+    playerSettings: {
+      hud: false
+    }
+  }).then((settings) => {
+    document.getElementById('hud').checked = settings.playerSettings.hud;
+  }).catch((error) => {
+    document.getElementById('hud').checked = false;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('hud').addEventListener('click', save_options);
+
